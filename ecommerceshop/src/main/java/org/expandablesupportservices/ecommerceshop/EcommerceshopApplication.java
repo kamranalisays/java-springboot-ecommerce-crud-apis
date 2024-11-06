365821a93cf8f98aa906f8ba4686b18beb24a27a
MASTER\allfo <kamranali.says@gmail.com>
2024-11-06 03:27:18 +0000

    initial spring boot project generated on https://start.spring.io/

---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/EcommerceshopApplication.java
@@ -0,0 +1,13 @@
+package org.expandablesupportservices.ecommerceshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceshopApplication.class, args);
+	}
+
+}
---
+++ ecommerceshop/src/test/java/org/expandablesupportservices/ecommerceshop/EcommerceshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.expandablesupportservices.ecommerceshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
