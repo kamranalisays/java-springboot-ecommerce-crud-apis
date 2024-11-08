e14ead85fdcc02ea808a526d2a93e5f8aef01d05
MASTER\allfo <kamranali.says@gmail.com>
2024-11-08 23:30:10 +0000

    Adding Product Entity .

--- ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/controllers/UserController.java
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.expandablesupportservices.ecommerceshop.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 }
---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/models/Product.java
@@ -0,0 +1,10 @@
+package org.expandablesupportservices.ecommerceshop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+}
