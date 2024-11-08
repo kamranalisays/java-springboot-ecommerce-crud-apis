31b6faed219fe680c977ac098141754430d72f57
MASTER\allfo <kamranali.says@gmail.com>
2024-11-08 23:25:48 +0000

    Adding ProductController , ProductRepository , UserRepository , ProductService  and UserService .

---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/repositories/ProductRepositoryI.java
@@ -0,0 +1,5 @@
+package org.expandablesupportservices.ecommerceshop.repositories;
+
+public interface ProductRepositoryI {
+
+}
---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/services/ProductService.java
@@ -0,0 +1,8 @@
+package org.expandablesupportservices.ecommerceshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
