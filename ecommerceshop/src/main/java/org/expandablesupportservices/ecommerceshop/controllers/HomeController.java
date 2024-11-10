6ba2392f73931043234ea0cf8a5647780d6cadf3
MASTER\allfo <kamranali.says@gmail.com>
2024-11-10 07:07:31 +0000

    Adding HomeController.

---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.expandablesupportservices.ecommerceshop.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	
+	
+	@GetMapping
+	public String getWelcomeMessage()
+	{
+		return "Welcome..!" ;
+	}
+	
+	
+
+}
