254a5e996a562c2be9e20acf61607f1739b8716b
MASTER\allfo <kamranali.says@gmail.com>
2024-11-18 02:45:43 +0000

    Changes for Payment.

--- ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/controllers/PaymentController.java
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/controllers/PaymentController.java
@@ -1,10 +1,34 @@
 package org.expandablesupportservices.ecommerceshop.controllers;
 
+import org.expandablesupportservices.ecommerceshop.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
+	
+
+	@Autowired
+	PaymentService paymentService;
+
+
+	@GetMapping("/all")
+	public ResponseEntity<?> getAllPayments() throws Exception  {
+
+		return paymentService.getAllPayments();
+
+	}
+
+	@GetMapping("/id/{paymentId}")
+	public ResponseEntity<?> getPaymentById(@PathVariable Long paymentId) {
+		return paymentService.getPaymentById(paymentId);
+
+	}
+
 
 }
---
+++ ecommerceshop/src/main/java/org/expandablesupportservices/ecommerceshop/services/PaymentService.java
@@ -0,0 +1,19 @@
+package org.expandablesupportservices.ecommerceshop.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+	public ResponseEntity<?> getAllPayments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResponseEntity<?> getPaymentById(Long paymentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
